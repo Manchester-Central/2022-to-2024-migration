4dbfac19b86fe1444222625a4dfd19dde3e1a2b9
CHAOS 131 <131.chaos@gmail.com>
2024-01-13 10:48:06 -0500

    migrated 2024-not working

---
+++ src/main/java/frc/robot/commands/DriverRelativeDrive.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import com.chaos131.gamepads.Gamepad;
+import frc.robot.subsystems.SwerveDrive;
+
+public class DriverRelativeDrive extends BaseRelativeDrive {
+
+  public DriverRelativeDrive(SwerveDrive drive, Gamepad controller) {
+    super(drive, controller);
+  }
+
+  @Override
+  public void moveRobot(double sidewaySpeed, double forwardSpeed, double thetaSpeed) {
+    m_drive.moveDriverRelative(-sidewaySpeed, forwardSpeed, thetaSpeed);
+  }
+}
